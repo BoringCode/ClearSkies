f1c22d12d087f8ceef888c6ed0ef288bec653e67
BoringCode <rosenfeld.bradley@gmail.com>
2014-07-28 23:34:57 -0400

    Added cardinal direction filter

--- build/js/ClearSkies.js
+++ build/js/ClearSkies.js
@@ -219,6 +219,48 @@ weatherApp.factory('Weather', ['$resource', '$rootScope', 'errors', 'Settings', 
 	}
 ]);
 
+weatherApp.filter("cardinalDirection", function() {
+	//convert weather data object to icon
+	return function (degrees) {
+		var maxDegree = 360;
+		var minDegree = 0;
+		if (degrees > maxDegree && degrees < minDegree) return "";
+		var directions = {
+			"N": [348.75, 11.25],
+			"NNE": [11.25, 33.75],
+			"NE": [33.75, 56.25],
+			"ENE": [56.25, 78.75],
+			"E": [78.75, 101.25],
+			"ESE": [101.25, 123.75],
+			"SE": [123.75, 146.25],
+			"SSE": [146.25, 168.75],
+			"S": [168.75, 191.25],
+			"SSW": [191.25, 213.75],
+			"SW": [213.75, 236.25],
+			"WSW": [236.25, 258.75],
+			"W": [258.75, 281.25],
+			"WNW": [281.25, 303.75],
+			"NW": [303.75, 326.25],
+			"NNW": [326.25, 348.75]
+		}
+		var finalDirection;
+		for (var direction in directions) {
+			var min = directions[direction][0];
+			var max = directions[direction][1];
+			if (min > max) {
+				if (degrees <= max || degrees >= min) {
+					finalDirection = direction;
+					break;
+				}
+			} else if (degrees <= max && degrees >= min) {
+				finalDirection = direction;
+				break;
+			}
+		}
+		return finalDirection;
+	}
+})
+
 weatherApp.filter('formatTime', function() {
 	return function(unix, format) {
 		return moment.unix(unix).format(format);
@@ -494,9 +536,26 @@ weatherApp.controller("WeatherCtrl", ['$scope', '$filter', 'errors', 'units', 'W
 ]).controller("Current", ['$scope',
 	function($scope) {
 		$scope.summary = "It's hot, but at least it isn't raining. You should put some sunscreen lotion on and go swimming!";
+		$scope.rainStatus = "No rain for the day";
+
+		$scope.minutelyStatus = function() {
+			var data = $scope.weather.data.minutely.data;
+			for (var i = 0; i < data.length; i++) {
+				var dataPoint = data[i];
+				if (dataPoint.precipIntensity > 0 && dataPoint.precipProbability > 0.5) {
+					break;
+				}
+			}
+			console.log(dataPoint);
+		}
+
 		//Stuff specific to current weather
 		$scope.$watchCollection("weather", function() {
-			$scope.predicted = $scope.weather.upcoming.data[0];
+			if ($scope.weather.loading === false) {
+				$scope.predicted = $scope.weather.upcoming.data[0];
+
+				$scope.minutelyStatus();
+			}
 		})
 	}
 ]).controller("Upcoming", ['$scope',
--- src/js/controllers/weather.controller.js
+++ src/js/controllers/weather.controller.js
@@ -9,9 +9,26 @@ weatherApp.controller("WeatherCtrl", ['$scope', '$filter', 'errors', 'units', 'W
 ]).controller("Current", ['$scope',
 	function($scope) {
 		$scope.summary = "It's hot, but at least it isn't raining. You should put some sunscreen lotion on and go swimming!";
+		$scope.rainStatus = "No rain for the day";
+
+		$scope.minutelyStatus = function() {
+			var data = $scope.weather.data.minutely.data;
+			for (var i = 0; i < data.length; i++) {
+				var dataPoint = data[i];
+				if (dataPoint.precipIntensity > 0 && dataPoint.precipProbability > 0.5) {
+					break;
+				}
+			}
+			console.log(dataPoint);
+		}
+
 		//Stuff specific to current weather
 		$scope.$watchCollection("weather", function() {
-			$scope.predicted = $scope.weather.upcoming.data[0];
+			if ($scope.weather.loading === false) {
+				$scope.predicted = $scope.weather.upcoming.data[0];
+
+				$scope.minutelyStatus();
+			}
 		})
 	}
 ]).controller("Upcoming", ['$scope',
