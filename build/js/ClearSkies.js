7d64ce76de3410939b3b6cb5cfa2e6de1266d797
BoringCode <rosenfeld.bradley@gmail.com>
2014-08-30 19:19:16 -0400

    Finished upcoming rain function

--- build/js/ClearSkies.js
+++ build/js/ClearSkies.js
@@ -533,28 +533,73 @@ weatherApp.controller("WeatherCtrl", ['$scope', '$filter', 'errors', 'units', 'W
 		$scope.location = GeoLocation;
 		$scope.units = units;
 	}
-]).controller("Current", ['$scope',
-	function($scope) {
+]).controller("Current", ['$scope', '$filter',
+	function($scope, $filter) {
 		$scope.summary = "It's hot, but at least it isn't raining. You should put some sunscreen lotion on and go swimming!";
-		$scope.rainStatus = "No rain for the day";
+		$scope.rainStatus = "";
+		$scope.rainStatusData = {};
 
-		$scope.minutelyStatus = function() {
-			var data = $scope.weather.data.minutely.data;
+		var types = {
+			"rain": ["Heavy rain", "Rain", "Rain showers", "Drizzle"],
+			"snow": ["Heavy snow", "Snow", "Light snow", "Flurries"]
+		}
+		$scope.upcomingRain = function() {
+			var rain = false;
+			var rainStarted = false;
+			var started = 0;
+			//Default status
+			var status = "No rain for the day";
+			//Create list of data (60 minutes and then 11 hours after that) 12 hours into the future
+			var data = [$scope.minutely, $scope.hourly.splice(1, 11)];
+			//Loop through two data sets
 			for (var i = 0; i < data.length; i++) {
-				var dataPoint = data[i];
-				if (dataPoint.precipIntensity > 0 && dataPoint.precipProbability > 0.5) {
-					break;
+				//Rain has been detected in previous data set, don't continue
+				if (rain) break;
+				//Get data set
+				var current = data[i];
+				for (var y = 0; y < current.length; y++) {
+					var dataPoint = current[y];
+					//Check to see if rain has started and whether it is likely to rain
+					if (rainStarted === false && dataPoint.precipIntensity > 0.017 && dataPoint.precipProbability > 0.5) {
+						rain = true;
+						status = "";
+						//Cloud cover is not included in data point, but is useful for forming the icon (just assume full cloud cover)
+						dataPoint.cloudCover = 1;
+						$scope.rainStatusData = dataPoint;
+						//Get descriptors based upon precip type (rain or snow at this point)
+						var descriptors = types[dataPoint.precipType];
+						if (dataPoint.precipIntensity >= 0.4) {
+							status += descriptors[0];
+						} else if (dataPoint.precipIntensity >= 0.1) {
+							status += descriptors[1];
+						} else if (dataPoint.precipIntensity >= 0.017) {
+							status += descriptors[2];
+						} else {
+							status += descriptors[3];
+						}
+						started = moment.unix(dataPoint.time);
+						status += " " + started.fromNow();
+						rainStarted = true;
+					//Check if rain has started and whether it stops at this data point
+					} else if (rainStarted === true && dataPoint.precipProbability < 0.5) {
+						//Create stopping message
+						status += ", ending " + moment.unix(dataPoint.time).from(started, true) + " later";
+						rainStarted = false;
+						break;
+					}
 				}
 			}
-			console.log(dataPoint);
+			//Set status in scope
+			$scope.rainStatus = status;
 		}
 
-		//Stuff specific to current weather
+		//Watch weather data object and update internal objects accordingly
 		$scope.$watchCollection("weather", function() {
 			if ($scope.weather.loading === false) {
 				$scope.predicted = $scope.weather.upcoming.data[0];
-
-				$scope.minutelyStatus();
+				$scope.minutely = $scope.weather.data.minutely.data;
+				$scope.hourly = $scope.weather.data.hourly.data;
+				$scope.upcomingRain();
 			}
 		})
 	}
